4b001a66cd3b126f999cafad0b9e9291b26b1d05
Patrick Crutchley <pcrutchl@psych.upenn.edu>
2013-11-20 17:13:09 -0500

    updates to eigenword fcns

--- code/learners/counts_logit_reg_train.m
+++ code/learners/counts_logit_reg_train.m
@@ -1,4 +1,4 @@
-function model = counts_logit_reg_train(train_labels,train_data,opts)
+function model_clr = counts_logit_reg_train(train_labels,train_data,opts)
 
 if ~exist('opts','var')
    opts = '-s 7 -q'; % best options through CV
@@ -6,6 +6,6 @@ end
 
 addpath liblinear-1.94/matlab
 
-model = train(train_labels,train_data,opts);
+model_clr = train(train_labels,train_data,opts);
 
 % produced RMSE 1.0314 on quiz set
